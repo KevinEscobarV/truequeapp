d1a70fb6a73866645fbc229f58c8a1487f0e8a2f
alexortiz <Sjhon1256@gmail.com>
2022-09-22 20:58:40 -0500

    Roles

---
+++ app/Http/Livewire/Admin/RolesComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use Livewire\Component;
+
+class RolesComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.roles-component');
+    }
+}
